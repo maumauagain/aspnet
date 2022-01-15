ad920aa40844e3cfad0eb05dc673ec7ce167985c
maumauagain <amauri.martins.jr@hotmail.com>
2022-01-15 11:32:23 -0300

    Adding PostBack Exercise

---
+++ Exercises/PostBack/Default.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="PostBack.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Choose your favorite programming languages<br />
+            <asp:CheckBoxList ID="chkList" runat="server">
+            </asp:CheckBoxList>
+            <br />
+            <asp:Button ID="btnOk" runat="server" OnClick="btnOk_Click" Text="Ok" />
+            <br />
+            <br />
+            <asp:Label ID="lblResult" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Exercises/PostBack/Default.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PostBack
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                chkList.Items.Add("C");
+                chkList.Items.Add("C++");
+                chkList.Items.Add("C#");
+                chkList.Items.Add("Visual Basic 6.0");
+                chkList.Items.Add("VB .NET");
+                chkList.Items.Add("Pascal");
+            }
+        }
+
+        protected void btnOk_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = "You chose: <br /> <b>";
+            foreach (ListItem item in chkList.Items)
+            {
+                if(item.Selected)
+                    lblResult.Text += item.Value + "<br />";
+            }
+
+            lblResult.Text += " </b>";
+        }
+    }
+}
