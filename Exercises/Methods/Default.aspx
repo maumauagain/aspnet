34aabea189ac3846966fa44897df1d51f8975445
maumauagain <amauri.martins.jr@hotmail.com>
2022-01-14 21:56:53 -0300

    Adding Calendar and Method exercise

---
+++ Exercises/Methods/Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Methods.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Length:<asp:TextBox ID="txtLength" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Width:<asp:TextBox ID="txtWidth" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="btnArea" runat="server" OnClick="btnArea_Click" Text="Area" />
+            <br />
+            <br />
+            <asp:Label ID="lblResult" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Exercises/Methods/Default.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Methods
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnArea_Click(object sender, EventArgs e)
+        {
+            double len;
+            double wid;
+            double area;
+
+            len = double.Parse(txtLength.Text);
+            wid = double.Parse(txtWidth.Text);
+
+            area = AreaMethod(len, wid);
+            lblResult.Text = area.ToString();
+        }
+
+        private double AreaMethod(double len, double wid)
+        {
+            double area;
+            area = len * wid;
+            return area;
+        }
+    }
+}
